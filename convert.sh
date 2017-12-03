d494de1353fe20bf67e1a16286ec346ef739fa0f
Rafael Castro <rafaelcg10@gmail.com>
2017-12-03 14:58:13 -0200

    add let

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default \
     -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
-    -dCompressFonts=true -r150 -sOutputFile=haskellgp.pdf main.pdf
+    -dCompressFonts=true -r150 -sOutputFile=rp.pdf main.pdf
