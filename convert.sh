3d783bbe33649aac7622fc7735b1bcd77888f21a
Rafael Castro <rafaelcg10@gmail.com>
2017-12-03 22:48:03 -0200

    compress file

--- convert.sh
+++
@@ -1,3 +0,0 @@
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default \
-    -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages \
-    -dCompressFonts=true -r150 -sOutputFile=rp.pdf main.pdf
